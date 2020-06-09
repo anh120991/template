04c4ff47304c3cbb9457c1a78de7b34c41480a71
anhnt9 <anhnt9@ocb.com.vn>
2020-06-09 17:28:16 +0700

    a

--- dolomia-html-template/HTML/assets/js/main.js
+++ dolomia-html-template/HTML/assets/js/main.js
@@ -569,4 +569,52 @@ $(document).ready(function($) {
     } catch (err) {
 
     }
+      $(document).ready(function(){
+  $('.my-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    dots: false,
+    merge:true,
+    nav: true,
+    pagination: false,
+    responsive:{
+    0:{
+      items: 2
+     },
+   600:{
+      items: 1
+                                }
+  }
+  });
+
+})
+
+    $('.material-card > .mc-btn-action').click(function () {
+            var card = $(this).parent('.material-card');
+            var icon = $(this).children('i');
+            icon.addClass('fa-spin-fast');
+
+            if (card.hasClass('mc-active')) {
+                card.removeClass('mc-active');
+
+                window.setTimeout(function() {
+                    icon
+                        .removeClass('fa-arrow-left')
+                        .removeClass('fa-spin-fast')
+                        .addClass('fa-bars');
+
+                }, 800);
+            } else {
+                card.addClass('mc-active');
+
+                window.setTimeout(function() {
+                    icon
+                        .removeClass('fa-bars')
+                        .removeClass('fa-spin-fast')
+                        .addClass('fa-arrow-left');
+
+                }, 800);
+            }
+        });
 });
